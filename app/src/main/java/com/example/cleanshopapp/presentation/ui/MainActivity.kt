9f85a0ec602d1b8ca3e2993168be5bcdd05aa5b6
Jakhongir Farkhodov <jahongirfarxodov@gmail.com>
2024-02-02 10:35:50 +0500

    Clean architecture layers has been created

--- app/src/main/java/com/example/cleanshopapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/cleanshopapp/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cleanshopapp
+package com.example.cleanshopapp.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cleanshopapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
