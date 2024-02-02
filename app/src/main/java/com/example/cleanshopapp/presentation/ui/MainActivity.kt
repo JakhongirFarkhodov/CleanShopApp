ef69a1935d0e1efa76d2987da1e8344675c1f7e5
Jakhongir Farkhodov <jahongirfarxodov@gmail.com>
2024-02-02 21:08:30 +0500

    MainViewModel has been created

--- app/src/main/java/com/example/cleanshopapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/cleanshopapp/presentation/ui/MainActivity.kt
@@ -2,14 +2,32 @@ package com.example.cleanshopapp.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.cleanshopapp.R
 import com.example.cleanshopapp.domain.model.main.ShopItem
+import com.example.cleanshopapp.presentation.viewmodel.main.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        var isDeleted = true
+        viewModel.getShopList().observe(this){
+            Log.d("TAG", "onCreate: ${it}")
+            if (isDeleted){
+                val item = it[1].copy(isEnabled = !it[1].isEnabled)
+                viewModel.editShopItem(item)
+                isDeleted = false
+            }
+        }
 
     }
 }
